3546ea727777c279d64b9644b61d348d4f2ddaed
Srayash Singh <146334722+Srayash@users.noreply.github.com>
2024-12-15 18:03:04 +0530

    Update Input.jsx

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -8,7 +8,7 @@ import { conversationAtom } from "../store/atoms/conversation";
 export default function Input({ placeholder }) {
   const [value, setValue] = useState("");
   const textareaRef = useRef(null);
-  // // for Development uncomment this.
+  // for Development uncomment this.
   // const backendUrl = 'http://localhost:2738'; 
   // for Production. (comment out for dev.)
   const backendUrl = 'https://retrogpt-backend.onrender.com';
